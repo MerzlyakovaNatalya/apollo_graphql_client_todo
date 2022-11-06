ccf23fd682f499d648dbf5b21b8cd62904982bb8
MerzlyakovaNatalya <94801202+MerzlyakovaNatalya@users.noreply.github.com>
2022-11-06 20:39:50 +0300

    add_query

--- src/App.js
+++ src/App.js
@@ -1,23 +1,28 @@
 import logo from './logo.svg';
-import './App.css';
+import AddTodo from './components/AddTodo';
+import TodoList from './components/TodoList';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
+    <div>
+      <header>
+      </header>
+      <main>
+      <div className='App'>
+        <AddTodo />
+        <TodoList />
+      </div>
+      </main>
+      <footer>
         <a
-          className="App-link"
+          
           href="https://reactjs.org"
           target="_blank"
           rel="noopener noreferrer"
         >
           Learn React
         </a>
-      </header>
+      </footer>
     </div>
   );
 }
---
+++ src/components/TodoItem.jsx
@@ -0,0 +1,25 @@
+import { Checkbox, Text, CloseButton, HStack } from '@chakra-ui/react';
+
+const TodoItem = ({ id, title, completed, onToggle }) => {
+
+  return (
+    <HStack spacing={3}>
+      <Checkbox
+        isChecked={completed}
+        onChange={() => {
+          onToggle({
+            variables: {
+              id,
+              completed: !completed
+            }
+          }
+          )
+        }}
+      />
+      <Text>{title}</Text>
+      <CloseButton />
+    </HStack>
+  );
+};
+
+export default TodoItem;
